593a621594dbed539de5d527e4b33f4e2cadcfb8
GấuMiLo <117769024+Dtrong101@users.noreply.github.com>
2023-10-04 00:47:37 +0700

    v2

---
+++ Stradale/Stardale HTML/js/enviroment.js
@@ -0,0 +1,11 @@
+    // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDA9q2THqTG2FDcugU_qggdT9dsxcL_ouc",
+    authDomain: "coffee-shop-7dd07.firebaseapp.com",
+    projectId: "coffee-shop-7dd07",
+    storageBucket: "coffee-shop-7dd07.appspot.com",
+    messagingSenderId: "587261396550",
+    appId: "1:587261396550:web:d3efd7274f47a00952ec47"
+  };
+   // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
